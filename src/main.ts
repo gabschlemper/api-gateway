718c6cbce75a5a958227dc6fdf230a8ac17a06c8
Gabriela Schlemper <gabrielaschlemper@icloud.com>
2026-06-17 08:37:10 -0300

    update default port to 3005 in main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3005;
 
   app.use(helmet());
 
